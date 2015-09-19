f831b20e5956c9a9ee328fdcb5acf3243d934445
unknown <pxl1778@C2435-22.main.ad.rit.edu>
2015-09-19 17:46:58 -0400

    Moving Object yay

---
+++ OpenTheDoorGame/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour {
+
+    private RaycastHit hit;
+    public GameObject other;
+	
+	// Update is called once per frame
+	void Update () {
+	     if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), out hit, 2f))
+         {
+             if(Input.GetMouseButtonDown(0) && hit.collider.gameObject == this.gameObject)
+             {
+                 other.transform.position = this.transform.position;
+                 Destroy(this.gameObject);
+             }
+         }
+	}
+}
